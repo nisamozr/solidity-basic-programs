4790bf0f27e2ba3c8783fb5187c1aec7b9a65864
nisamozr <codeexplorkl10@gmail.com>
2021-11-07 20:39:50 +0530

    sample and multipication using function

---
+++ sample.sol
@@ -0,0 +1,18 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+
+contract smaple {
+    constructor(){
+        
+    }
+    // function to return two numbers sum
+    
+    function sum() public view returns(uint){
+        uint a = 10;
+        uint b= 5;
+        
+        return a+b;
+        
+    }
+    
+}
