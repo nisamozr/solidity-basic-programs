deeb9b4685c14286afc0d4a8090d80c414efbd48
nisamozr <codeexplorkl10@gmail.com>
2021-11-07 20:59:16 +0530

    sample and multipication using function

---
+++ function.sol
@@ -0,0 +1,11 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+
+contract functionSample{
+    
+        function multipication(uint a , uint b) public view returns(uint){
+        uint resulte = a*b;
+        return resulte;
+        
+    }
+}
