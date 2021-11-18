f14609fa049683439acd58cd06276b9098c3b5e7
nisamozr <codeexplorkl10@gmail.com>
2021-11-18 20:41:17 +0530

    abstract

---
+++ hello.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.7.0 < 0.9.0 ;
+
+
+contract hello{
+    
+    string public name ;
+
+    function (string memory _name) public returns(string memory){
+       return name = _name;
+    }
+    //saple
+    
+}
+
